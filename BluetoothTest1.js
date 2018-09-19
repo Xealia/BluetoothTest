00dc8022bc6c65010001d8cd416ae8c6ff41a8a6
Xealia <lillylar93@outlook.com>
2018-09-19 09:33:03 +0200

    commented in

--- BluetoothTest1.js
+++ BluetoothTest1.js
@@ -3,36 +3,36 @@ var log = document.getElementById('output');
 function onButtonClick() {
     log.innerHTML = 'Hei!';
 
-    //let filters = [];
+    let filters = [];
 
-    //let filterService = document.querySelector('#service').value;
-    //if (filterService.startsWith('0x')) {
-    //    filterService = parseInt(filterService);
-    //}
-    //if (filterService) {
-    //    filters.push({ services: [filterService] });
-    //}
+    let filterService = document.querySelector('#service').value;
+    if (filterService.startsWith('0x')) {
+        filterService = parseInt(filterService);
+    }
+    if (filterService) {
+        filters.push({ services: [filterService] });
+    }
 
-    //let filterName = document.querySelector('#name').value;
-    //if (filterName) {
-    //    filters.push({ name: filterName });
-    //}
+    let filterName = document.querySelector('#name').value;
+    if (filterName) {
+        filters.push({ name: filterName });
+    }
 
-    //let filterNamePrefix = document.querySelector('#namePrefix').value;
-    //if (filterNamePrefix) {
-    //    filters.push({ namePrefix: filterNamePrefix });
-    //}
+    let filterNamePrefix = document.querySelector('#namePrefix').value;
+    if (filterNamePrefix) {
+        filters.push({ namePrefix: filterNamePrefix });
+    }
 
-    //let options = {};
-    //if (document.querySelector('#allDevices').checked) {
-    //    options.acceptAllDevices = true;
-    //} else {
-    //    options.filters = filters;
-    //}
+    let options = {};
+    if (document.querySelector('#allDevices').checked) {
+        options.acceptAllDevices = true;
+    } else {
+        options.filters = filters;
+    }
 
-    //console.log('Requesting Bluetooth Device...');
-    //console.log('with ' + JSON.stringify(options));
-    navigator.bluetooth.requestDevice({ service: "heart_rate" })
+    log.innerHTML('Requesting Bluetooth Device...');
+    log.innerHTML('with ' + JSON.stringify(options));
+    navigator.bluetooth.requestDevice(options)
         .then(device => {
             log.innerHTML = '> Name:             ' + device.name;
             log.innerHTML += '> Id:               ' + device.id;
